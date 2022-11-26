d2d192c32779fcb60ab6ed876e70dc94429cd4bd
Goncalo-Palma-Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2022-11-26 12:37:45 +0000

    Fixed merge conflicts

--- hangman_client.cpp
+++ hangman_client.cpp
@@ -10,11 +10,6 @@ Gonçalo Nunes - ist199229
 Mateus Pinho - ist199282
 */
 
-// main function that takes two arguments from the command line
-// the first argument is the server's IP address
-// the second argument is the server's port number
-
-// this should have something in it but i dont remember how include files work
 #include "hangman_client.hpp"
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,8 +23,6 @@ Mateus Pinho - ist199282
 #include <cstring>
 #include <unistd.h>
 #include <iostream>
-
-// TODO don't add \n from hint to file
 
 /* Constants */
 #define BLOCK_SIZE 256
@@ -68,30 +61,31 @@ int move_number = 1;
 std::string player_id;
 std::string word = "_____";
 
-int start_new_game(std::string id);
-int valid_id(std::string id);
-int receive_message(int fd, socklen_t addrlen, sockaddr_in addr, char buffer[]);
+int start_new_game(std::string id, int fd, struct addrinfo *res, struct sockaddr_in addr);
+//int valid_id(std::string id);
+int receive_message(int fd, sockaddr_in addr, char* buffer, size_t buf_size);
 int send_message(int fd, char message[], size_t buf_size, struct addrinfo *res);
-int exit_game(char message[], int fd, size_t buf_size, struct addrinfo *res);
-std::string play_result(char message[]);
-int play_aux_ok(char message[], char letter);
-int play(int fd, socklen_t addrlen, struct sockaddr_in addr, char message[], char letter);
+int exit_game(int fd, struct addrinfo *res);
+std::string play_result(std::string message);
+int play_aux_ok(std::string message, char letter);
+int play(int fd, struct sockaddr_in addr, char message[], char letter);
 std::string format_word();
+
 
 
 // TODO add checks to see if the move_numbers make sense when communicating with the server
 // TODO sprintf is always used alongside send_message. Maybe abstract it
 
+
 int main(int argc, char *argv[]) {
     int fd, errorcode;
-    char* err;
-    //ssize_t n;
+    ssize_t n;
     socklen_t addrlen;
     struct addrinfo hints, *res;    // TODO custom name for structs
     struct sockaddr_in addr;
     char buffer[BLOCK_SIZE];
 
-    // Check if the number of arguments is correct
+    // check if the number of arguments is correct
     if (argc != 3)
     {
         printf("Input Error: Incorrect number of arguments.\nExpected 2 arguments, received %d\n", argc - 1);
@@ -99,17 +93,17 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // Get the server's IP address and port number
+    // get the server's IP address and port number
     char *server_ip = argv[1];
     char *server_port = argv[2];
 
-    // Create a socket
+    // create a socket
     fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd == -1) {
         exit(1);
     }
 
-    // Set the server's address
+    // set the server's address
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
@@ -119,76 +113,23 @@ int main(int argc, char *argv[]) {
         exit(1); // TODO set error message + close socket
     }
 
-    /* Main program code */
-    char* input = (char *) malloc(INPUT * sizeof(char));
-    if (input == NULL) {
-        exit(1); // TODO error message + close socket
-    }
-
-    char* command = (char *) malloc(COMMAND_SIZE * sizeof(char));
-    if (input == NULL) {
-        exit(1); // TODO error message + close socket
-    }
-
-    /* Client application loop */
+    /* main program code */
     
-    int terminate = FALSE;
-    while (terminate == FALSE) {
-        err = fgets(input, INPUT, stdin);
-        if (err == NULL) {
-            // TODO maybe check ferror too
-            printf("Error (main): Failed to correctly read input with fgets.");
-        }
+    while (1) {
+        std::string input;
+        std::getline(std::cin, input);
 
+        // split input in two strings using space as delimiter, doesnt account for malformed input
+        std::string command = input.substr(0 , input.find(' '));
+        std::string message = input.substr(input.find(' ') + 1, input.length());
         // check if the command is equal to "start"
-
-        if (!strcmp(command, START)) {
-            //start_new_game(message);
-        }
-        else if (!strcmp(command, QUIT) || !strcmp(command, EXIT)) {
-            exit_game(buffer, fd, BLOCK_SIZE, res);
-
-            if (!strcmp(command, EXIT)) {
-                terminate = TRUE;
-            }
-        }
-        else {
-            printf("Error (main): Invalid command\n");
+        if (command == "start") {
+            start_new_game(message, fd, res, addr);
         }
     }
 
-
-    free(input);
-    free(command);
     freeaddrinfo(res);
     close(fd);
-}
-
-int receive_message(int fd, socklen_t addrlen, struct sockaddr_in addr, char buffer[]) {
-    addrlen = sizeof(addr);
-    ssize_t n = recvfrom(fd, buffer, BLOCK_SIZE, 0, (struct sockaddr*)&addr, &addrlen);
-    
-    if(n==-1) { 
-        /* Error */
-        printf("Error (receive_message): An error occured while receiving a message.\n");
-        printf("    error message was received from ssize_t recvfrom(int sockfd, void *restrict buf, size_t len, int flags, struct sockaddr *restrict src_addr, socklen_t *restrict addrlen)\n");
-        return -1;    // TODO error message
-    }
-
-    return 0;
-}
-
-int send_message(int fd, char message[], size_t buf_size, struct addrinfo *res) {
-    ssize_t n = sendto(fd, message, buf_size, 0, res->ai_addr, res->ai_addrlen);
-
-    if(n == -1) {
-        /* Error */
-        printf("Error (send_message): An error occured while sending a message.\n");
-        printf("    error message was received from ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)\n");
-        return -1;    // TODO error message
-    }
-
-    return 0;
 }
 
 std::string format_word() {
@@ -205,44 +146,79 @@ std::string format_word() {
     return formatted_word;
 }
 
-int start_new_game(std::string id) {
+int receive_message(int fd, struct sockaddr_in addr, char* buffer, size_t buf_size) {
+    socklen_t addrlen = sizeof(addr);
+    ssize_t n = recvfrom(fd, buffer, buf_size, 0, (struct sockaddr*)&addr, &addrlen);
 
-    if (!valid_id(id)) {
-        // TODO send error message
-        return -1;
+    if(n==-1) { 
+        /* Error */
+        printf("Error (receive_message): An error occured while receiving a message.\n");
+        printf("    error message was received from ssize_t recvfrom(int sockfd, void *restrict buf, size_t len, int flags, struct sockaddr *restrict src_addr, socklen_t *restrict addrlen)\n");
+        exit(1);    // TODO error message
     }
 
-    // Connect to server
-
-    // Send ID and new game request
-
-    // Receive message
-
-    // Set word size
-
-    // Set maximum number of errors
-
-    return 0; // 0 is a placeholder
-}
-
-int valid_id(std::string id) {
-    int i = 0;
-
-    if (id.size() != 6) {
-        return -1;
-    }
-
-    while (id[i] != '\0' || id[i] != '\n') {
-        if (!isdigit(id[i])) {
-            return -1;
-        }
-        i++;
-    }
     return 0;
 }
 
+int send_message(int fd, const char* message, size_t buf_size, struct addrinfo *res) {
+    ssize_t n = sendto(fd, message, buf_size, 0, res->ai_addr, res->ai_addrlen);
+
+    if(n == -1) {
+        /* Error */
+        printf("Error (send_message): An error occured while sending a message.\n");
+        printf("    error message was received from ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)\n");
+        exit(1);    // TODO error message
+    }
+
+    return 0;
+}
+
+int start_new_game(std::string id, int fd, struct addrinfo *res, struct sockaddr_in addr) {
+    // Send ID and new game request
+    player_id = id;
+    std::string message = "SNG " + id + "\n";
+    send_message(fd, message.c_str(), message.length(), res);
+
+    // TODO Receive message
+    // ! fix the buffer being block size
+    char buffer[BLOCK_SIZE];
+    receive_message(fd, addr, buffer, BLOCK_SIZE);
+    std::string response = buffer;
+
+    // split input in four strings using space as delimiter, doesnt account for malformed input
+    // remove \n from response
+    response.pop_back();
+
+    std::string response_command = response.substr(0 , response.find(' '));
+    if (response_command == "ERR") {
+        printf("Error.\n");
+        return -1;
+    }
+    
+    std::string status = response.substr(response.find(' ') + 1, response.find(' ', response.find(' ') + 1));
+    std::string n_letters = response.substr(response.find(' ', response.find(' ') + 1) + 1, response.find(' ', response.find(' ', response.find(' ') + 1) + 1));
+    std::string max_errors = response.substr(response.find(' ', response.find(' ', response.find(' ') + 1) + 1) + 1, response.length());
+
+    // convert number of letters into a number of underscores to print
+    int n_letters_int = std::stoi(n_letters);
+    std::string underscores = "";
+    for (int i = 0; i < n_letters_int; i++) {
+        underscores += "_ ";
+    }
+
+    // ! code down sucks
+    // // if (status != "OK") {
+    // //     // TODO send error message
+    // //     return -1;
+    // // }
+
+    printf("New game started (max %s errors): %s\n", max_errors.c_str(), underscores.c_str());
+    
+    return 0; // 0 is a placeholder
+}
+
 // TODO maybe rename to play_status
-std::string play_result(char message[]) {
+std::string play_result(std::string message) {
     int i = 4; // Skip reply signature (RLG) and space
 
     std::string status;
@@ -254,7 +230,7 @@ std::string play_result(char message[]) {
     return status;
 }
 
-int play_aux_ok(char message[], char letter) {
+int play_aux_ok(std::string message, char letter) {
     int i = 8; // Skip to trial number () RLG OK
     int trial_number = 0;
     while (message[i] != ' ') {
@@ -288,22 +264,23 @@ int play_aux_ok(char message[], char letter) {
     return 0;
 }
 
-int play(int fd, socklen_t addrlen, struct addrinfo *res, struct sockaddr_in addr,
-        char message[], size_t buf_size, char letter) {
+int play(int fd, struct addrinfo *res, struct sockaddr_in addr,
+        char buffer[], size_t buf_size, char letter) {
 
     // Send message
-    int err = sprintf(message, "%s %s %c %d", QUT, player_id.c_str(), letter, move_number);
-    if (err < 0) {
+    std::string message = PLG + player_id + letter + std::to_string(move_number) + '\n';
+    // // int err = sprintf(message, "%s %s %c %d", QUT, player_id.c_str(), letter, move_number);
+    /*if (err < 0) {
         printf("Error (play): An error occured while preparing the exit message\n");
-    }
+    }*/
 
-    err = send_message(fd, message, buf_size, res);
+    int err = send_message(fd, message.c_str(), message.length(), res);
     if (err == -1) {
         printf("Error (play): An error occured while sending the exit message\n");
     }
 
     // Receive message
-    err = receive_message(fd, addrlen, addr, message);
+    err = receive_message(fd, addr, buffer, buf_size);
     if (err == -1) {
         printf("Error (play): An error occured while receiving a message from the server\n");
     }
@@ -340,14 +317,11 @@ int play(int fd, socklen_t addrlen, struct addrinfo *res, struct sockaddr_in add
     return 0;
 }
 
-int exit_game(char message[], int fd, size_t buf_size, struct addrinfo *res) {
+int exit_game(int fd, struct addrinfo *res) {
 
-    int err = sprintf(message, "%s %s", QUT, player_id.c_str());
-    if (err < 0) {
-        printf("Error (exit_game): An error occured while preparing the exit message\n");
-    }
+    std::string message = QUT + player_id + '\n';
 
-    err = send_message(fd, message, buf_size, res);
+    int err = send_message(fd, message.c_str(), message.length(), res);
     if (err == -1) {
         printf("Error (exit_game): An error occured while sending the exit message\n");
     }
@@ -356,3 +330,19 @@ int exit_game(char message[], int fd, size_t buf_size, struct addrinfo *res) {
 
     return 0;
 }
+
+// // int valid_id(std::string id) {
+// //     int i = 0;
+
+// //     if (id.length() != 6) {
+// //         return -1;
+// //     }
+
+// //     while (id[i] != '\0' || id[i] != '\n') {
+// //         if (!isdigit(id[i])) {
+// //             return -1;
+// //         }
+// //         i++;
+// //     }
+// //     return 0;
+// // }
