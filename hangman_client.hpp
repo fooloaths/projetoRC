4791bbc355ccc9e59dd13b24612cb269efb2c1f1
Mateus Pinho <mateusleitepinho@gmail.com>
2022-12-20 17:50:33 +0000

    back again

--- hangman_client.hpp
+++ hangman_client.hpp
@@ -33,3 +33,20 @@
 #define REV "REV "
 #define rev "rev "
 #define VICTORY_MESSAGE "WELL DONE! You guessed: "
+
+void start_new_game(std::string id, int fd, struct addrinfo *res, struct sockaddr_in addr);
+std::string receive_message(int fd, sockaddr_in addr, size_t buf_size);
+void send_message(int fd, char message[], size_t buf_size, struct addrinfo *res);
+void exit_game(std::string id, int fd, struct addrinfo *res, struct sockaddr_in addr);
+std::string get_status(std::string message);
+std::string play_aux_ok(std::string message, std::string letter);
+void play(std::string letter, int fd, struct addrinfo *res, struct sockaddr_in add);
+std::string format_word(std::string word_to_format = word);
+void scoreboard(const char* server_ip, const char* server_port);
+void guess(std::string word, int fd, struct addrinfo *res, struct sockaddr_in addr);
+void reveal_word(int fd, struct addrinfo *res, struct sockaddr_in addr);
+void status(const char* server_ip, const char* server_port);
+void hint(const char* server_ip, const char* server_port);
+void status_aux_ok(std::string message);
+void hint_aux_ok(std::string message);
+std::string tcp_helper(std::string message, const char* server_ip, const char* server_port);
